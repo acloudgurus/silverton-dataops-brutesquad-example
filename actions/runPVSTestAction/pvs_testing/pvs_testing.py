bdaa8b1c5fc49fdb2e4e94889bbb0eecae7ab801
acloudgurus <66688065+acloudgurus@users.noreply.github.com>
2025-05-09 12:31:51 +0530

    Update pvs_testing.py

--- actions/runPVSTestAction/pvs_testing/pvs_testing.py
+++ actions/runPVSTestAction/pvs_testing/pvs_testing.py
@@ -12,6 +12,10 @@ from datetime import datetime
 import pandas as pd
 import teradatasql
 import logging
+import sys
+import glob
+import json
+import re
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -19,10 +23,9 @@ logger = logging.getLogger(__name__)
 
 
 # Executes SQL query given against td_conn passed into function
-def _execute_tdv_query(td_conn, query):
+def execute_tdv_query(td_conn, query):
     query_result = pd.read_sql(query, td_conn)
     logger.info({'query': query, 'result': query_result})
-
     try:
         return query_result.to_dict()
     except Exception as e:
@@ -39,48 +42,126 @@ def _pass_or_fail(result_dict):
         exit(1)
 
 
-# Main function to perform PVS Test against specified stored procedures
+# Fetch files from the tables folder
+def _fetch_all_sql_files(base_folder):
+    tables_path = os.path.join(base_folder, 'tables')
+    sql_files = []
+
+    if not os.path.exists(tables_path):
+        logger.info(f"No tables directory in {base_folder}")
+        return []
+
+    for filename in os.listdir(tables_path):
+        if filename.endswith(".sql"):
+            sql_files.append(os.path.join(tables_path, filename))
+
+    return sql_files
+
+
+# Extracting the stored procedure name
+def _extract_proc_names_from_file(filepath):
+    extracted_procs = []
+
+    with open(filepath, 'r') as f:
+        file_content = f.read()
+
+    pattern = r'(?i)\b(create|replace|call)\s+(procedure\s+)?(\S+\$\{dbEnv\}\.\S+)'
+    matches = re.findall(pattern, file_content)
+
+    for match in matches:
+        proc_name = match[2]
+        extracted_procs.append(proc_name)
+
+    return extracted_procs
+
+
+def _get_final_proc_list(folder_list):
+    final_proc_list = []
+
+    for folder in folder_list:
+        logger.info(f"Searching in Folder: {folder}")
+        sql_files = _fetch_all_sql_files(folder)
+        logger.info(f"Found {len(sql_files)} SQL files in {folder}")
+        for sql_file in sql_files:
+            procs = _extract_proc_names_from_file(sql_file)
+            if procs:
+                logger.info(f"Extracted from {sql_file}: {procs}")
+                final_proc_list.extend(procs)
+
+    proc_set = set()
+    logger.info("\n==== FINAL LIST OF PROCEDURES/FUNCTIONS FOUND ====")
+    for proc in final_proc_list:
+        string_proc = str(proc).replace("${dbEnv}.", "_" + os.environ.get("TDV_ENV") + ".")
+        string_proc = string_proc + "()"
+        proc_set.add(string_proc)
+
+    procs_clean = list(proc_set)
+    logger.info("PROCS CLEAN: ")
+    for x in procs_clean:
+        logger.info(x)
+
+    return procs_clean
+
+
+def _run_pvs_test(procs_clean, work_item_id, td_conn, tdv_username):
+    start_test_procedure = f"CALL PVS_TEST.START_PVS_TEST('{tdv_username}','{work_item_id}',PROC_MSG)"
+    end_test_procedure = f"CALL PVS_TEST.END_PVS_TEST('{tdv_username}','{work_item_id}',PROC_MSG)"
+    pvs_table_result_query = f"select TEST_STATUS from PVS_TEST.PVS_TEST_INFO_V where USER_NAME = '{tdv_username}' and WORK_ITEM = '{work_item_id}'"
+
+    logger.info(f"Executing Start PVS Test")
+    execute_tdv_query(td_conn=td_conn, query=start_test_procedure)
+
+    for sp in procs_clean:
+        logger.info(f"Executing Stored Procedure: {sp}")
+        execute_tdv_query(td_conn=td_conn, query="CALL " + sp)
+
+    logger.info(f"Executing End PVS Test")
+    execute_tdv_query(td_conn=td_conn, query=end_test_procedure)
+
+    logger.info(f"Result of PVS Test")
+    pvs_result = execute_tdv_query(td_conn=td_conn, query=pvs_table_result_query)
+    _pass_or_fail(pvs_result)
+
+
 def main():
-    logger.info(f"Hello from the script")
-    work_item_id = str(datetime.now().strftime("%Y%m%d%H%M"))
+    folder_list = os.environ.get("FOLDER_LIST")
+    teradata_dir_list = os.environ.get("DIRECTORY_LIST")
+    logger.info(f"teradata_folder_list: {teradata_dir_list}")
 
-    ### DO NOT TRY TO PRINT SECRETS - GITHUB MASKS THESE VALUES WHEN PRINTED
-    teradata_username = os.environ.get("TDV_USERNAME")
-    teradata_password = os.environ.get("TDV_PASSWORD")
+    if not folder_list:
+        logger.info("FOLDER_LIST not found in env")
+        return
 
-    teradata_host_server = "hstntduat.healthspring.inside"
+    try:
+        folder_list = json.loads(folder_list)
+    except Exception as e:
+        logger.info("Failed to parse FOLDER_LIST:", e)
+        return
 
-    ## TODO: Changing workitem id
-    work_item_id = "CHG77777_CTASK77777:"
-    pvs_table_result_query = f"select TEST_STATUS from PVS_TEST.PVS_TEST_INFO_V where USER_NAME = 'SVT_DATAOPS_DEV' and WORK_ITEM = '{work_item_id}'"
-    start_test_procedure = f"CALL PVS_TEST.START_PVS_TEST('SVT_DATAOPS_DEV','{work_item_id}',PROC_MSG)"
-    end_test_procedure = f"CALL PVS_TEST.END_PVS_TEST('SVT_DATAOPS_DEV','{work_item_id}',PROC_MSG)"
-    stored_procedure = "CALL HSETL_WORK_DEV.BLUEPRINT_SAMPLE_SPROC()"
+    folder_list = [folder for folder in folder_list if folder.strip()]
+    logger.info(f"folder_list: {str(folder_list)}")
 
-    with teradatasql.connect(
-            host=teradata_host_server,
-            user=teradata_username,
-            password=teradata_password,
-            LOGMECH="LDAP",
-            encryptdata=True
-    ) as td_conn:
-        # Start PVS Test
-        logger.info(f"Executing Start PVS Test")
-        _execute_tdv_query(td_conn=td_conn, query=start_test_procedure)
+    procs_clean = _get_final_proc_list(folder_list)
 
-        # Run stored procedure(s)
-        logger.info(f"Executing Stored Procedure: {stored_procedure}")
-        _execute_tdv_query(td_conn=td_conn, query=stored_procedure)
+    # teradata_username = os.environ.get("TDV_USERNAME")
+    # teradata_password = os.environ.get("TDV_PASSWORD")
+    # teradata_host_server = "hstntduat.healthspring.inside"
 
-        # End PVS Test
-        logger.info(f"Executing End PVS Test")
-        _execute_tdv_query(td_conn=td_conn, query=end_test_procedure)
+    # change_num = os.environ.get("ChangeTicket_Num")
+    # task_num = os.environ.get("CTASK_NUM")
+    # work_item_id = f"CHG{change_num}_CTASK{task_num}"
+    # logger.info(f"WorkitemID: {work_item_id}")
 
-        # Get results
-        logger.info(f"Result of PVS Test")
-        pvs_result = _execute_tdv_query(td_conn=td_conn, query=pvs_table_result_query)
-        _pass_or_fail(pvs_result)
+    # with teradatasql.connect(
+    #     host=teradata_host_server,
+    #     user=teradata_username,
+    #     password=teradata_password,
+    #     LOGMECH="LDAP",
+    #     encryptdata=True
+    # ) as td_conn:
+    #     _run_pvs_test(procs_clean, work_item_id, td_conn, teradata_username)
 
 
 if __name__ == "__main__":
     main()
+
